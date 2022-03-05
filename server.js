7f94618898750099adfc9bb11948b7cce6f528c6
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-03-04 20:42:57 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -12,13 +12,27 @@ app.use((req, res, next) => {
     next();
 });
 
+//near the top, around other app.use() calls
+app.use(express.urlencoded({extended:false}));
+
+//put this above your Show route
+app.get('/pokemon/new', (req, res) => {
+    res.render('New');
+});
+ 
+app.get('/pokemon', function(req, res){
+    res.render('Index', { pokemon: pokemon });
+});      
+
 // app.get('/', function(req,res){
 //     res.send(' <h1>Welcome to the Pokemon App!</h1>')
 //   })
 
-  app.get('/pokemon', function(req,res){
-    res.send(pokemon)
-  })
+//   app.get('/pokemon', function(req,res){
+//     res.send(pokemon)
+//   })
+
+
   
   // Tell the app to listen on port 3000
   // for HTTP requests from clients
---
+++ views/Index.jsx
@@ -0,0 +1,42 @@
+const React = require('react');
+
+const myStyle = {
+    color: 'black',
+    backgroundColor: 'grey',     
+  };
+
+  const anchorStyle = {
+    style : 'text-transform:capitalize',
+  }
+
+const pokemon = require('../models/pokemon');
+
+    class Index extends React.Component {
+      render() {
+          const { poke } = this.props;
+          return (
+                  <div style={myStyle}>
+                      <h1>See All The Pokemon!</h1>
+                      <ul>
+                          {pokemon.map((poke, i) => {
+                              return (
+                                  <li>
+                                      <h1>The Pokemon name is {' '}    
+                                        <a href={poke.img}>
+                                          {poke.name}
+                                       
+                                    </a>{' '}
+                                    </h1>         
+                                  </li>
+                              );
+                          })}
+                      </ul>
+                      {/* <nav>
+                        <a href="/fruits/new">Create a New Fruit</a>
+                      </nav> */}
+                  </div>
+          );
+      }
+    }
+
+    module.exports = Index;
